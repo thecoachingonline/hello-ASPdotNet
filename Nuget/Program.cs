b5f185473d7637a68e76b3b3b1c25b49bdf254e3
Y.Nanthachai <thecoachingonline@gmail.com>
2023-11-27 03:29:26 +0000

    NuGet package

---
+++ Nuget/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Humanizer;
+
+namespace Nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TimeSpan.FromMilliseconds(1299630020).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(3603001).Humanize(3));
+            Console.WriteLine(TimeSpan.FromMilliseconds(8157019263).Humanize(3));
+        }
+    }
+}
